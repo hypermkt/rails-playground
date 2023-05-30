befe6265362b975137f95ac3398e24c837ef176b
hypermkt <hypermkt@gmail.com>
2023-05-30 23:29:42 +0900

    Response with JSON

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,19 +1,7 @@
 class Api::V1::ItemsController < ApplicationController
   def index
     items = Item.all
-    items_messages = items.map {|item| build_item_message(item)}
 
-    send_data(::ItemsResponse.encode(::ItemsResponse.new(
-      items: items_messages
-    )))
-  end
-
-  private
-
-  def build_item_message(item)
-    ::ItemResponse.new(
-      id: item.id,
-      name: item.name
-    )
+    render json: items
   end
 end
