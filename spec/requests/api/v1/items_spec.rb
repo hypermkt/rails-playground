b6c4c37c7e2a6533213362e9824e129ad68b6a57
hypermkt <hypermkt@gmail.com>
2023-05-30 23:41:58 +0900

    Fix test

--- spec/requests/api/v1/items_spec.rb
+++ spec/requests/api/v1/items_spec.rb
@@ -4,11 +4,8 @@ RSpec.describe 'Api::V1::Item', type: :request do
   describe '#index' do
     let!(:item) { create(:item) }
     before { get api_v1_items_url }
-    it 'response with protocol buffers' do
-      items_response = ::ItemsResponse.decode(response.body)
-      expect(response).to have_http_status(200)
-      expect(items_response.items[0].id).to eq item.id
-      # expect(items_response.items[0].title).to eq item.title
-    end
+
+    it { expect(response.status).to eq(200) }
+    it { expect(json.size).to eq 1 }
   end
 end
