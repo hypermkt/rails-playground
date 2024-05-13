493beaec084ecbd6eea9848af5d8f0769f9eb100
hypermkt <hypermkt@gmail.com>
2024-05-13 22:51:39 +0900

    Add name

--- db/migrate/20231203141836_create_posts.rb
+++ db/migrate/20231203141836_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-
+      t.string :name, null: false
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_05_30_140735) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_03_141836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,12 @@ ActiveRecord::Schema[7.1].define(version: 2023_05_30_140735) do
   end
 
   create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
