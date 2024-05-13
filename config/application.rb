e2d60014f41637d5ee20a5d29e0ab3412b666af1
hypermkt <hypermkt@gmail.com>
2024-05-13 23:07:30 +0900

    CI上でのテスト落ちを解決するため

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,9 @@ module RailsWithGrpc
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    # 自動でパスに追加するため
+    config.add_autoload_paths_to_load_path = true
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
