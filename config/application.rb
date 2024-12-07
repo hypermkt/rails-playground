23bcc6329f575b31ca44ce181b57b49bef03c7ab
hypermkt <hypermkt@gmail.com>
2024-12-07 23:33:38 +0900

    日本時間にする

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,8 @@ module RailsWithGrpc
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Asia/Tokyo'
   end
 end
